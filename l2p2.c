492e9d41dccaa00c24d306325579604cb7cb0c12
Sebi Coroian <escapemcro@gmail.com>
2020-03-03 18:07:47 +0200

    Update l2p2.c

--- l2p2.c
+++ l2p2.c
@@ -1,22 +1,10 @@
-#include<stdlib.h>
 #include<stdio.h>
 int main()
 {
 	unsigned int n,s=0;
 	scanf("%d", &n);
-while(n>0)
-{
-	s+=n%10;
-	n/=10;
-}
-	
+	for(int i=n;i>0;i/=10)
+		s+=i%10;
 	printf("%d\n", s);
 }
 
-/*
-
-%d -- int
-%f -- float
-%c -- char
-
-*/
